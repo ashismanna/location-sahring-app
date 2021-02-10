81df9a82b39aed6d5740db1f1b09ed16c91057a4
Ashis Manna <ashis.manna@tcs.com>
2021-02-10 15:16:01 +0530

    seed file prepare and postgres add for heroku deployment

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,10 +4,16 @@
 # Examples:
 #
 
-10.times{ |t,|
+10.times{ |t|
 	user = User.create(:username=>"user_#{t}",:firstname=>"Sample",:lastname=>"User #{t}",:email=>"email#{t}@example.com",:password=>"tcs12345")
-	# location = Location.create(:latitude=>81.99884943770512+(t*3), :longitude=>24.759262454384654+(t*4))
+	location = Location.create(:latitude=>81.99884943770512+(t*2), :longitude=>24.759262454384654+(t*3))
 	# SharedLocation.create(:user_id=>user.id,:location_id=>location.id,)
+
+}
+user_ids = User.all.collect(&:id)
+location_ids = Location.all.collect(&:id)
+9.times{|t|
+	(t/2==0) ? SharedLocation.create(:user_id=>user_ids[t],:location_id=>location_ids.sample,:is_public=>1,:target_user=>user_ids.sample) : SharedLocation.create(:user_id=>user_ids[t],:location_id=>location_ids.sample,:is_public=>0,:target_user=>user_ids.sample)
 }
 
 
